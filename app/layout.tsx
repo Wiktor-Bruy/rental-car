ef7881d7ee04b39df5bf381384ecb3cec36b758a
Crazy_Fox <holmyliszag@ukr.net>
2026-05-12 15:51:37 +0300

    init project

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next';
+import { Manrope, Inter } from 'next/font/google';
+import 'modern-normalize';
+import './globals.css';
+
+const geistMan = Manrope({
+  variable: '--font-family',
+  subsets: ['latin'],
+});
+
+const geistInter = Inter({
+  variable: '--second-family',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Car rental website home page',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${geistMan.variable} ${geistInter.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// import css from './page.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <p>Hme</p>
+    </>
+  );
+}
